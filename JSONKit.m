e097f39e511ad197fbeb0d9ed245dda9d53e2fa1
Jean-Charles BERTIN <jc.bertin@axinoe.com>
2012-10-18 18:22:04 +0200

    Added JK_REMOVE_DEPRECATED macro to remove all deprecated stuff.

--- JSONKit.m
+++ JSONKit.m
@@ -2203,6 +2203,7 @@ static id _JKParseUTF8String(JKParseState *parseState, BOOL mutableCollections, 
 #pragma mark Deprecated as of v1.4
 ////////////
 
+#ifndef JK_REMOVE_DEPRECATED
 // Deprecated in JSONKit v1.4.  Use objectWithUTF8String:length: instead.
 - (id)parseUTF8String:(const unsigned char *)string length:(size_t)length
 {
@@ -2226,6 +2227,7 @@ static id _JKParseUTF8String(JKParseState *parseState, BOOL mutableCollections, 
 {
   return([self objectWithData:jsonData error:error]);
 }
+#endif
 
 ////////////
 #pragma mark Methods that return immutable collection objects
